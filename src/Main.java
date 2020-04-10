e7b43a01f439f2bdba23e6be248d78eea46b27ea
Alex Pelaez <pelaezalex8@gmail.com>
2020-04-10 15:50:59 -0400

    Racetracks are read in correctly & update gitignore to not include /out

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        Racetrack r = new Racetrack();
+        Racetrack r = new Racetrack("/Users/Alex/Documents/School/AI/Artificial-Intelligence-Reinforcement-Learning-Racetrack/src/inputs/R-track.txt");
     }
 }
--- src/Racetrack.java
+++ src/Racetrack.java
@@ -1,5 +1,43 @@
-/**
- * Created by Alex on 4/10/20.
- */
+import java.io.File;
+import java.util.Scanner;
+
 public class Racetrack {
+    private char[][] racetrack;
+    private File file;
+
+    public Racetrack(String filePath){
+        this.file = new File(filePath);
+        createTrack();
+        printTrack();
+    }
+
+    private void createTrack() {
+        try{
+            Scanner s = new Scanner(file);
+            int index = 0;
+            while (s.hasNextLine()) {
+                String currentLine = s.nextLine();
+                if(currentLine.charAt(0)!='#'){
+                    String[] firstLineSplit = currentLine.split(",");
+                    racetrack = new char[Integer.parseInt(String.valueOf(firstLineSplit[0]))]
+                            [Integer.parseInt(String.valueOf(firstLineSplit[1]))];
+                } else {
+                    racetrack[index] = currentLine.toCharArray();
+                    index++;
+                }
+            }
+        } catch (Exception e){
+
+        }
+
+    }
+    private void printTrack() {
+        for(int i = 0; i < racetrack.length; i++){
+            for(int j = 0; j < racetrack[0].length; j++){
+                System.out.print(racetrack[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
 }
